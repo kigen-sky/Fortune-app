cb66f9720436dc6316ad8ee00173f26be059b85a
unknown <Victor Kigen>
2024-07-08 20:25:53 +0300

    added a statements

--- app.js
+++ app.js
@@ -3,6 +3,17 @@ function tellFortune() {
 
     let favcolor = document.getElementById("favColor").Value;
     let career;
+
+    if(favcolor === "red") {
+        career = "You will be a pilot";
+    } else if (favcolor === "blue"){ 
+        career = "You will be a police";
+    }else if ( favcolor === "green"){
+        career = " You will be a software developer";
+    }else {
+        "Try another color"
+    }
+
 }
 
 
